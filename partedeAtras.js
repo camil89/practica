cfc76aad0f0a9b498eacb6ef81329e2bb66c6187
valentina-ovando <valentinaovando702@gmail.com>
2025-09-12 15:23:24 -0300

    cambio de estilo y agregar boton para poner tu dibujo

--- partedeAtras.js
+++ partedeAtras.js
@@ -1,60 +1,62 @@
-<body>
-    <div class="container container-gallery">
-        <div class="row g-4">
+function Pagina(){
+    <body>
+        <div class="container container-gallery">
+            <div class="row g-4">
 
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <img src="https://static.vecteezy.com/system/resources/previews/000/273/888/original/vector-abstract-pastel-background.jpg"
-                        class="img-thumbnail" alt="Imagen 1"></img>
-                    <div class="color-pastel"> Modo pastel</div>
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img src="https://static.vecteezy.com/system/resources/previews/000/273/888/original/vector-abstract-pastel-background.jpg"
+                            class="img-thumbnail" alt="Imagen 1"></img>
+                        <div class="color-pastel"> Modo pastel</div>
+                    </div>
                 </div>
-            </div>
 
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <img src="https://www.inabaweb.com/wp-content/uploads/2023/03/Comentarios-en-redes-sociales.png"
-                        class="img-thumbnail" alt="Imagen 2"></img>
-                    <div class="opiniones">Escribiendo tus opiniones</div>
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img src="https://www.inabaweb.com/wp-content/uploads/2023/03/Comentarios-en-redes-sociales.png"
+                            class="img-thumbnail" alt="Imagen 2"></img>
+                        <div class="opiniones">Escribiendo tus opiniones</div>
+                    </div>
                 </div>
-            </div>
 
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <img src="https://tse3.mm.bing.net/th/id/OIP.s-BV4wi-lycEK4naWwKWJgHaE7?pid=Api&P=0&h=180"
-                        class="img-thumbnail" alt="Imagen 3"></img>
-                    <div class="flores">dibujos de flores</div>
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img src="https://tse3.mm.bing.net/th/id/OIP.s-BV4wi-lycEK4naWwKWJgHaE7?pid=Api&P=0&h=180"
+                            class="img-thumbnail" alt="Imagen 3"></img>
+                        <div class="flores">dibujos de flores</div>
+                    </div>
                 </div>
-            </div>
 
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <img src="https://tse1.mm.bing.net/th/id/OIP.jS8Cn0iFQYC2_bx6vrT6fgHaE8?pid=Api&P=0&h=180"
-                        class="img-thumbnail" alt="Imagen 4"></img>
-                    <div class="tradicional">arte tradicional</div>
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img src="https://tse1.mm.bing.net/th/id/OIP.jS8Cn0iFQYC2_bx6vrT6fgHaE8?pid=Api&P=0&h=180"
+                            class="img-thumbnail" alt="Imagen 4"></img>
+                        <div class="tradicional">arte tradicional</div>
+                    </div>
                 </div>
-            </div>
 
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <img src="https://tse2.mm.bing.net/th/id/OIP.6_oSCkieJUSCtvz08fUeaQHaEO?pid=Api&P=0&h=180"
-                        class="img-thumbnail" alt="Imagen 5"></img>
-                    <div class="digital">arte digital</div>
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img src="https://tse2.mm.bing.net/th/id/OIP.6_oSCkieJUSCtvz08fUeaQHaEO?pid=Api&P=0&h=180"
+                            class="img-thumbnail" alt="Imagen 5"></img>
+                        <div class="digital">arte digital</div>
+                    </div>
                 </div>
-            </div>
 
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                <div class="card">
-                    <img src="https://i.ytimg.com/vi/C_QetK0D7c0/maxresdefault.jpg" class="img-thumbnail"
-                        alt="Imagen 6"></img>
-                    <div class="abstracto">arte abstracto</div>
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img src="https://i.ytimg.com/vi/C_QetK0D7c0/maxresdefault.jpg" class="img-thumbnail"
+                            alt="Imagen 6"></img>
+                        <div class="abstracto">arte abstracto</div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="d-grid gap-2">
+        <div class="d-grid gap-2">
 
-        <div class="d-grid gap-2 col-6 mx-auto">
-            <button class="btn btn-primary" type="button">publica tus dibujos</button>
+            <div class="d-grid gap-2 col-6 mx-auto">
+                <button class="btn btn-primary" type="button">publica tus dibujos</button>
+            </div>
         </div>
-    </div>
-</body>
+    </body>
+}
